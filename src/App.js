057dd5c6810d92d5956796c27f735c9baf874af4
Irann Orecchio <129894177+IrannnOrecchio@users.noreply.github.com>
2023-11-20 22:10:30 -0300

    Revert "Revert "system ticket implemented""

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import RoutesApp from "./Routes";
-
+import RoutesApp from "./Routes/Routes";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css";
+import AuthProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      <RoutesApp/>
-     
+      <BrowserRouter>
+        <AuthProvider>
+          <ToastContainer autoClose={3000} theme="colored" />
+          <RoutesApp />
+        </AuthProvider>
+      </BrowserRouter>
     </div>
   );
 }
